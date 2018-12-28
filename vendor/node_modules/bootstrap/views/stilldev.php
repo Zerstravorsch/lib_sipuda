275b82190a86a1b4aa6211d2e8b0461d278884d4
Zerstravorsch <45928430+Zerstravorsch@users.noreply.github.com>
2018-12-28 11:31:34 +0800

    Add files via upload

---
+++ vendor/node_modules/bootstrap/views/stilldev.php
@@ -0,0 +1,25 @@
+<?php
+$page_title = 'Halaman Masih Dalam Tahap Pengembangan';
+include('../pages/header.php');
+?>
+<body>
+    <?php include('../pages/navbar.php'); ?>
+      <!-- gambar header -->
+  <div class="py-5 text-center text-md-right section-parallax">
+    <br><br>
+    <div class="container-fluid" align="center" style="	max-height: 100px;">
+      <img align="center" width="120px" height="150px" border="5px" src="../assets/images/und.png">
+        <div class="py-2">
+          <div class="container-fluid" style="	max-height: 78px;">
+            <div class="row justify-content-md-center">
+              <h1 class="display-4 text-center" contenteditable="false" align="center">Maaf, Halaman Masih Dalam Tahap Pengembangan!</h1>
+              <br>
+            </div>
+            <div class="container-fluid" align="center">
+              <p align="center"> Silahkan klik <a href="../index">disini</a> untuk ke halaman utama SIPUDA</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</body>
